8b59f888a93241f18f9cc243d13012dfff6ba89c
pkrisz0 <p.krisz89@gmail.com>
2017-09-11 13:50:31 +0200

    Excercise 8: Swap

---
+++ week-02/day-01/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args){
+        int a = 123;
+        int b = 526;
+
+        a = 26;
+        b = 56;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
