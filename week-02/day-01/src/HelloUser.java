64164bca3b67eb5985535c104b2e96a03345627a
pkrisz0 <p.krisz89@gmail.com>
2017-09-11 15:10:02 +0200

    Excercise 14: Hello User

---
+++ week-02/day-01/src/HelloUser.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+        System.out.println("Hello, User! Please type in your username");
+
+        Scanner username = new Scanner(System.in);
+
+        String userInput1 = username.nextLine();
+
+        System.out.println("Hello," + userInput1 + "!");
+
+
+
+
+    }
+}
