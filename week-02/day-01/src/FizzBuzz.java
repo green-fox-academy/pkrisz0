44f58e962c488b40dc7aea41fb96dd335206ab1a
pkrisz0 <p.krisz89@gmail.com>
2017-09-11 16:38:22 +0200

    Excercise 27: FizzBuzz fixed

--- week-02/day-01/src/FizzBuzz.java
+++ week-02/day-01/src/FizzBuzz.java
@@ -10,13 +10,14 @@ public class FizzBuzz {
         while (a < 100) {
             if (a % 3 == 0 && a % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (a % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (a % 3 == 0){
+            } else if (a % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            System.out.println(a);
+            } else if (a % 5 == 0){
+                System.out.println("Buzz");
+            } else {
+            System.out.println(a);}
             a += 1;
+
         }
 
 
