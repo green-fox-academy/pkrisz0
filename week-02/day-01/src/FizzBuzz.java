b1af1fb32532606e98ac247c6d442229522bc184
pkrisz0 <p.krisz89@gmail.com>
2017-09-11 16:19:42 +0200

    Excercise 27: FizzBuzz

---
+++ week-02/day-01/src/FizzBuzz.java
@@ -0,0 +1,24 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Write a program that prints the numbers from 1 to 100.
+        // But for multiples of three print “Fizz” instead of the number
+        // and for the multiples of five print “Buzz”.
+        // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+        int a = 0;
+
+        while (a < 100) {
+            if (a % 3 == 0 && a % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (a % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (a % 3 == 0){
+                System.out.println("Fizz");
+            }
+            System.out.println(a);
+            a += 1;
+        }
+
+
+    }
+}
