330a93baea85bed393a8e6f92b8350ee153b1947
pkrisz0 <p.krisz89@gmail.com>
2017-09-11 13:41:47 +0200

    Excercise 7: My Favourite Number

---
+++ week-02/day-01/src/FavouriteNumber.java
@@ -0,0 +1,6 @@
+public class FavouriteNumber {
+    public static void main(String[] args){
+        int fav_number = 9;
+        System.out.println("My favourite number is: " + fav_number);
+    }
+}
