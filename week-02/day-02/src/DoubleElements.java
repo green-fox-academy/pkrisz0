577722e09fd196320fb8e14fe990895b4b60e674
pkrisz0 <p.krisz89@gmail.com>
2017-09-12 16:21:14 +0200

    Excercise 14: Double elements

---
+++ week-02/day-02/src/DoubleElements.java
@@ -0,0 +1,22 @@
+public class DoubleElements {
+    public static void main(String[] args) {
+        int[] ag = {3, 4, 5, 6, 7};
+
+        for(int i = 0; i < ag.length; i++) {
+            System.out.println(ag[i]);
+        }
+
+        for(int i = 0; i < ag.length; i++){
+            ag[i] = ag[i] * 2;
+        }
+
+        for(int i = 0; i < ag.length; i++) {
+            System.out.println(ag[i]);
+        }
+
+    }
+}
+
+// - Create an array variable named `ag`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
