14a5526ad1a0e2eb0bb3524c2cf595a71a9489f6
pkrisz0 <p.krisz89@gmail.com>
2017-09-13 15:54:35 +0200

    fixed the name

---
+++ week-02/day-03/src/ToDoPrint.java
@@ -0,0 +1,19 @@
+public class ToDoPrint {
+    public static void main(String[] args) {
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        char[] letters = new char[reversed.length()];
+
+        for(int i = 0; i < reversed.length(); i++){
+            letters[i] = reversed.charAt(i);
+        }
+
+        for(int i = 0; i < reversed.length(); i++) {
+            System.out.print(letters[(letters.length - 1) - i]);
+        }
+    }
+}
+
+// Create a function that can reverse a String, which is passed as the parameter
+// Use it on this reversed string to check it!
+// Try to solve this using charAt() first, and optionally anything else after.
