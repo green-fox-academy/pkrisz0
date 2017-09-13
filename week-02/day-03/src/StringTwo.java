9b4950f685e0e58af49eb4d8cc6ec95216bed70e
pkrisz0 <p.krisz89@gmail.com>
2017-09-13 11:21:45 +0200

    Excercise 2: String Replacement with multiple solution

--- week-02/day-03/src/StringTwo.java
+++ week-02/day-03/src/StringTwo.java
@@ -2,11 +2,22 @@ public class StringTwo {
     public static void main(String[] args) {
         String url = "https//www.reddit.com/r/nevertellmethebots";
 
+        // Solution #1
+
         String urlfixed = url.replace("https", "https:");
 
         String odds = urlfixed.replace("bots", "odds");
 
         System.out.println(odds);
+
+        // Solution #2
+
+        String http = "https:";
+        String ends = "odds";
+
+        String shorty = url.substring(5,38);
+
+        System.out.println(http + shorty + ends);
     }
 }
 
