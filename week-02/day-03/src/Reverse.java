aeff6ef6997ad140e2255e2ffc56b4a73dc97d9f
pkrisz0 <p.krisz89@gmail.com>
2017-09-13 22:18:04 +0200

    made it simpler, put into function tomorrow !

--- week-02/day-03/src/Reverse.java
+++ week-02/day-03/src/Reverse.java
@@ -3,17 +3,13 @@ public class Reverse {
         String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
         // solution 1
-        char[] letters = new char[reversed.length()];
-
-        for(int i = 0; i < reversed.length(); i++){
-            letters[i] = reversed.charAt(i);
-        }
-
+        
         String dummytwo = "";
 
-        for(int i = reversed.length() - 1; i > 0 ; i--) {
-             char dummy = (letters[i]);
-             dummytwo = dummytwo.concat(String.valueOf(dummy));
+        for(int i = reversed.length() - 1; i > 0 ; i--){
+            char dummy = reversed.charAt(i);
+
+            dummytwo = dummytwo.concat(String.valueOf(dummy));
         }
 
         reversed = dummytwo;
