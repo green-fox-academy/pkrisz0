f17b4ab8368a6718db0ebbdd42b31561cdbb1a38
pkrisz0 <p.krisz89@gmail.com>
2017-09-13 15:13:30 +0200

    Excercise 4: To-do list concatenation

--- week-02/day-03/src/StringFour.java
+++ week-02/day-03/src/StringFour.java
@@ -2,7 +2,7 @@ public class StringFour {
     public static void main(String[] args) {
         String todoText = "\n - Buy milk\n";
 
-        String todoAll = todoText.concat(" - Download games\n".concat(" - Diablo\n"));
+        String todoAll = todoText.concat(" - Download games\n".concat("\t - Diablo\n"));
 
         System.out.println("My todo" + todoAll);
     }
