d73bf120bb3fa903494a0f96e07a354b5a3438f4
pkrisz0 <p.krisz89@gmail.com>
2017-09-13 14:26:44 +0200

    Excercise 10: find element in an arraylist

---
+++ week-02/day-03/src/ElementFinder.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ElementFinder {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+
+        // Solution #1
+        if (arrayList.contains(7) == true){
+            System.out.println("Hooray");
+        } else {
+            System.out.println("Noooooo");
+        }
+
+        //Solution #2
+
+        //hm
+
+    }
+}
+
+// Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+// Do this again with a different solution using different list functions!
+
