b837f2d1390c48e984c8109da91a95baadc43e74
papp_kriszti <31337285+pkrisz0@users.noreply.github.com>
2017-09-13 16:04:22 +0200

    formatting

--- week-02/day-03/src/TakesLonger.java
+++ week-02/day-03/src/TakesLonger.java
@@ -3,7 +3,6 @@ public class TakesLonger {
         String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
 
         //positive lookaround for the win!!! will totally demo this
-
         String[] parts = quote.split("(?<=t )",2);
 
         String partone = parts[0];
@@ -20,3 +19,4 @@ public class TakesLonger {
 // When saving this quote a disk error has occured. Please fix it.
 // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
 // Using pieces of the quote variable (instead of just redefining the string)
+
