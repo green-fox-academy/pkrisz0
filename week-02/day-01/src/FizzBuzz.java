4cece2ccb6bed610e6ccb6c404f0e023fea5cf90
pkrisz0 <p.krisz89@gmail.com>
2017-09-17 15:30:38 +0200

    Exercise 27: FizzBuzz + formatting

--- week-02/day-01/src/FizzBuzz.java
+++ week-02/day-01/src/FizzBuzz.java
@@ -1,25 +1,21 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        // Write a program that prints the numbers from 1 to 100.
-        // But for multiples of three print “Fizz” instead of the number
-        // and for the multiples of five print “Buzz”.
-        // For numbers which are multiples of both three and five print “FizzBuzz”.
-
-        int a = 0;
-
-        while (a < 100) {
-            if (a % 3 == 0 && a % 5 == 0) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (a % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (a % 5 == 0){
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
-            System.out.println(a);}
-            a += 1;
-
+                System.out.println(i);
+            }
         }
-
-
     }
 }
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
