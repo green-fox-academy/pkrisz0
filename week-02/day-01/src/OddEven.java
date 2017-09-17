bc2acad05dfe11e0ed39eecaaff2b05cfd1517d4
papp_kriszti <31337285+pkrisz0@users.noreply.github.com>
2017-09-17 14:17:19 +0200

    Exercise 18: Oddeven + formatting

--- week-02/day-01/src/OddEven.java
+++ week-02/day-01/src/OddEven.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class OddEven {
-    public static void main(String[] args) {
-
-        // Write a program that reads a number form the standard input,
-        // Than prints "Odd" if the number is odd, or "Even" it it is even.
-
+    public static void main(String[] args) {       
+        
         System.out.println("Please enter a random number:");
-        Scanner number = new Scanner(System.in);
-        int userInput = number.nextInt();
+        Scanner userInput = new Scanner(System.in);
+        int number = userInput.nextInt();
 
-        if (userInput % 2 == 0) {
+        if (number % 2 == 0) {
             System.out.println("You have entered and even number!");
         } else {
-                System.out.println("You have entered an odd number!");
-            }
+            System.out.println("You have entered an odd number!");
         }
-    }
+   }
+}
+
+// Write a program that reads a number form the standard input,
+// Than prints "Odd" if the number is odd, or "Even" it it is even.
+
