beb9ffec80c6e575b998aa8d866c4025f59710c0
papp_kriszti <31337285+pkrisz0@users.noreply.github.com>
2017-09-17 13:53:39 +0200

    Exercise 13: Seconds in a day corrected

--- week-02/day-01/src/SecondsInADay.java
+++ week-02/day-01/src/SecondsInADay.java
@@ -7,9 +7,9 @@ public class SecondsInADay {
         // Write a program that prints the remaining seconds (as an integer) from a
         // day if the current time is represented bt the variables
 
-        // ido most 14:34:42
+        // time now 14:34:42
 
-        int remaining = (60 - currentSeconds) + ((60 - currentMinutes) * 60) + ((24 - currentHours) * 60 * 60);
+        int remaining = (60 - currentSeconds) + ((59 - currentMinutes) * 60) + ((23 - currentHours) * 60 * 60);
         System.out.println(remaining);
     }
 }
