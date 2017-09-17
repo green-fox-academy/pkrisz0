1e7409b61ed62b669c32190d27e884fec0b8b957
papp_kriszti <31337285+pkrisz0@users.noreply.github.com>
2017-09-17 13:58:20 +0200

    Exercise 14: HelloUser + formatting

--- week-02/day-01/src/HelloUser.java
+++ week-02/day-01/src/HelloUser.java
@@ -4,16 +4,12 @@ public class HelloUser {
     public static void main(String[] args) {
         // Modify this program to greet user instead of the World!
         // The program should ask for the name of the user
+        
         System.out.println("Hello, User! Please type in your username");
 
         Scanner username = new Scanner(System.in);
-
         String userInput1 = username.nextLine();
 
         System.out.println("Hello," + userInput1 + "!");
-
-
-
-
     }
 }
