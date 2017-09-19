b04dda036ea721051e99a1b82c7b3225d7062526
pkrisz0 <p.krisz89@gmail.com>
2017-09-19 16:12:21 +0200

    Exercise 7: Duplicator simplified

--- week-03/day-02/src/Doubled.java
+++ week-03/day-02/src/Doubled.java
@@ -11,54 +11,34 @@ public class Doubled {
         decoder("src/duplicated-chars.txt");
     }
 
-    public static String decoder (String inputFile) {
+    public static void decoder (String inputFile) {
         Path filepath = Paths.get(inputFile);
         String fin = "";
         try {
             List<String> content = Files.readAllLines(filepath);
-
-            //adding each line to an array
-            String[] lines = new String[content.size()];
-            for (int i = 0; i < content.size(); i++) {
-                lines[i] = content.get(i);
-            }
+            List<String> noduplicates = new ArrayList<>();
 
             //removing duplications from each line
-            String[] noduplication = new String[lines.length];
             String result = "";
-            for (int j = 0; j < lines.length; j++) {
-                for (int i = 0; i < lines[j].length(); i++) {
+            for (int j = 0; j < content.size(); j++) {
+                for (int i = 0; i < content.get(j).length(); i++) {
                     if (i == 0) {
-                        result += String.valueOf(lines[j].charAt(i));
-                    } else if (i > 0 && lines[j].charAt(i) != lines[j].charAt(i - 1)) {
-                        result += String.valueOf(lines[j].charAt(i));
+                        result += String.valueOf(content.get(j).charAt(i));
+                    } else if (i > 0 && content.get(j).charAt(i - 1) != content.get(j).charAt(i))  {
+                        result += String.valueOf(content.get(j).charAt(i));
                     }
                 }
-
-                noduplication[j] = result;
+                noduplicates.add(result);
                 result = "";
             }
 
-
-            //turn array into arraylist for printing
-
-            List<String> contents = new ArrayList();
-
-            for (int i = 0; i < noduplication.length; i++) {
-                contents.add(noduplication[i]);
-            }
-
-
             //print to new
             Path filePathOut = Paths.get("src/duplicated-chars-decoded.txt");
-            Files.write(filePathOut, contents);
-
+            Files.write(filePathOut, noduplicates);
 
         } catch (IOException e) {
-
+            System.out.println("Something went wrong :(");
         }
-
-        return fin;
     }
 }
 
