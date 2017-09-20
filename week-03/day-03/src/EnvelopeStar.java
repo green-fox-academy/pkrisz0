45d156f2d39194272bcb879b68139af4fedeb5d2
pkrisz0 <p.krisz89@gmail.com>
2017-09-20 17:11:59 +0200

    Exercise 17: Envelope star complete

--- week-03/day-03/src/EnvelopeStar.java
+++ week-03/day-03/src/EnvelopeStar.java
@@ -9,27 +9,34 @@ public class EnvelopeStar {
     public static void mainDraw(Graphics graphics){
 
         //top right numbers to be incremented
-        int y1tr = 0;
+        int y1tr = 10;
         int x2tr = WIDTH / 2;
         //bottom right to be incremented
-        int y1br = HEIGHT;
+        int y1br = HEIGHT - 10;
         int x2br = WIDTH / 2;
+        //bottom left to be incremented
+        int x1bl =0;
+        int y2bl = HEIGHT / 2 + 20;
+        //top left to be incremented
+        int x1tl = 0;
+        int y2tl = HEIGHT / 2 - 20;
 
         for (int i = 0; i < WIDTH / 40; i++) {
             graphics.setColor(Color.GREEN);
             graphics.drawLine(WIDTH / 2, y1tr, x2tr, HEIGHT / 2); //top right
             graphics.drawLine(WIDTH / 2, y1br, x2br, HEIGHT / 2); //bottom right
-
+            graphics.drawLine(x1bl,HEIGHT / 2, WIDTH / 2, y2bl); //bottom left
+            graphics.drawLine(x1tl,HEIGHT / 2, WIDTH / 2, y2tl); //top left
 
             y1tr += 20;
             x2tr += 20;
             y1br -= 20;
             x2br += 20;
-
-
-        }
-
-
+            x1bl += 20;
+            y2bl += 20;
+            x1tl += 20;
+            y2tl -= 20;
+       }
     }
 
 //    Don't touch the code below
