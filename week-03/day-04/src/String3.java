18c54e3a6ac8435600a20f16d797d986b25a23a1
pkrisz0 <p.krisz89@gmail.com>
2017-09-21 15:19:21 +0200

    Exercise 9: String 3 + *

---
+++ week-03/day-04/src/String3.java
@@ -0,0 +1,19 @@
+public class String3{
+    public static void main(String[] args) {
+        System.out.println(xtoy("MadMaxxxxxx"));
+    }
+
+    static String xtoy (String input) {
+        String output = "";
+        if (input.length() < 1) {
+            return output;
+        } else if (input.charAt(0) == input.charAt(0)) {
+            return output = input.charAt(0) + "*" + xtoy(input.substring(1));
+        }
+        return output = input.charAt(0) + xtoy(input.substring(1));
+    }
+}
+
+
+// Given a string, compute recursively a new string where all the
+// adjacent chars are now separated by a "*".
