86194630d687d4fa41a1f97a1a8568cecac997f2
pkrisz0 <p.krisz89@gmail.com>
2017-09-25 16:46:47 +0200

    Exercise 9: Student & Teacher

---
+++ week-04/day-01/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    String name;
+
+    public void learn() {
+
+    }
+
+    public void question(Teacher teacher) {
+        teacher.answer();
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ week-04/day-01/src/Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher {
+    public void teach(Student student){
+        student.learn();
+    }
+
+    public void answer(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
