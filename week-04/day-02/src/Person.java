0493399f65afe6aabd80f4858b751749d35dbe84
pkrisz0 <p.krisz89@gmail.com>
2017-09-26 15:07:51 +0200

    Exercise 1: Green Fox inheritance exercise complete

--- week-04/day-02/src/Person.java
+++ week-04/day-02/src/Person.java
@@ -16,7 +16,7 @@ public class Person {
     }
 
     public void introduce() {
-        System.out.println("Hi, I'm " +  name + " a " + age + " year old "+ gender);
+        System.out.println("Hi, I'm " +  name + " a " + age + " year old " + gender + " .");
     }
 
     public void getGoal() {
