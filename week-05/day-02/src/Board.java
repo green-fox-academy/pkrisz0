d2896c9f594c9c1f29864f92d766ac382b205dca
pkrisz0 <p.krisz89@gmail.com>
2017-10-03 16:18:16 +0200

    turning the hero

--- week-05/day-02/src/Board.java
+++ week-05/day-02/src/Board.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Board extends JComponent implements KeyListener {
 
@@ -13,7 +11,7 @@ public class Board extends JComponent implements KeyListener {
     int posY;
     int boardSide;
     int imgSide;
-
+    String display;
 
     public Board() {
         testBoxX = 0;
@@ -21,49 +19,51 @@ public class Board extends JComponent implements KeyListener {
         posX = 0;
         posY = 0;
         boardSide = 720;
-        imgSide = 70;
+        imgSide = 72;
+        display = "C:/greenfox/pkrisz0/week-05/day-02/Images/hero-down.png";
 
         // set the size of your draw board
         setPreferredSize(new Dimension(boardSide, boardSide));
         setVisible(true);
     }
 
+    //map for wall
+    int[][] walls = new int[][]{
+            {0,0,0,1,0,1,0,0,0,0},  //0
+            {0,0,0,1,0,1,0,1,1,0},  //1
+            {0,1,1,1,0,1,0,1,1,0},  //2
+            {0,0,0,0,0,1,0,0,0,0},  //3
+            {1,1,1,1,0,1,1,1,1,0},  //4
+            {0,1,0,1,0,0,0,0,1,0},  //5
+            {0,1,0,1,0,1,1,0,1,0},  //6
+            {0,1,0,0,0,0,0,0,1,0},  //7
+            {0,0,0,1,0,1,1,0,1,0},  //8
+            {0,1,0,1,0,1,0,0,0,0}   //9
+    };
+
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
-        graphics.fillRect(testBoxX, testBoxY, imgSide, imgSide);
+        // graphics.fillRect(testBoxX, testBoxY, imgSide, imgSide);
         // here you have a 720x720 canvas
         // you can create and draw an image using the class below e.g.
 
         //draw board with walls
 
-        int[][] walls = new int[][]{
-                {0,0,0,1,0,1,0,0,0,0},  //0
-                {0,0,0,1,0,1,0,1,1,0},  //1
-                {0,1,1,1,0,1,0,1,1,0},  //2
-                {0,0,0,0,0,1,0,0,0,0},  //3
-                {1,1,1,1,0,1,1,1,1,0},  //4
-                {0,1,0,1,0,0,0,0,1,0},  //5
-                {0,1,0,1,0,1,1,0,1,0},  //6
-                {0,1,0,0,0,0,0,0,1,0},  //7
-                {0,0,0,1,0,1,1,0,1,0},  //8
-                {0,1,0,1,0,1,0,0,0,0}   //9
-        };
-
         for (int j = 0; j < walls.length; j++) {
-           for (int i = 0; i < walls.length; i++) {
-               if (walls[j][i] == 1) {
-                   PositionedImage wall = new PositionedImage("C:/greenfox/pkrisz0/week-05/day-02/Images/wall.png", i * imgSide, j * imgSide);
-                   wall.draw(graphics);
-               } else if (walls[j][i] == 0) {
-                   PositionedImage background = new PositionedImage("C:/greenfox/pkrisz0/week-05/day-02/Images/floor.png", i * imgSide, j * imgSide);
-                   background.draw(graphics);
-               }
-           }
+            for (int i = 0; i < walls.length; i++) {
+                if (walls[j][i] == 1) {
+                    PositionedImage wall = new PositionedImage("C:/greenfox/pkrisz0/week-05/day-02/Images/wall.png",imgSide * i,imgSide * j);
+                    wall.draw(graphics);
+                } else if (walls[j][i] == 0) {
+                    PositionedImage background = new PositionedImage("C:/greenfox/pkrisz0/week-05/day-02/Images/floor.png", imgSide * i, imgSide * j);
+                    background.draw(graphics);
+                }
+            }
         }
 
         //hero
-        PositionedImage hero = new PositionedImage("C:/greenfox/pkrisz0/week-05/day-02/Images/hero-down.png", testBoxX, testBoxY);
+        PositionedImage hero = new PositionedImage(display, testBoxX, testBoxY);
         hero.draw(graphics);
 
     }
@@ -101,12 +101,15 @@ public class Board extends JComponent implements KeyListener {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             testBoxY -= imgSide;
+            display = "C:/greenfox/pkrisz0/week-05/day-02/Images/hero-up.png";
         } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
             testBoxY += imgSide;
         } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
             testBoxX -= imgSide;
+            display = "C:/greenfox/pkrisz0/week-05/day-02/Images/hero-left.png";
         } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
             testBoxX += imgSide;
+            display = "C:/greenfox/pkrisz0/week-05/day-02/Images/hero-right.png";
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
