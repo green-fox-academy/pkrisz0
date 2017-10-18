4213c353c77434c718df4c479ee3d34e0d61804d
pkrisz0 <p.krisz89@gmail.com>
2017-10-18 16:49:07 +0200

    lottery complete

--- week-06/day-03/lotteryfilter/src/main/java/MainTwo.java
+++ week-06/day-03/lotteryfilter/src/main/java/MainTwo.java
@@ -11,39 +11,54 @@ import java.util.List;
 public class MainTwo {
 
     public static void main(String[] args) {
-        execute(args);
+        argHandler(args);
     }
 
-    public static void execute(String[] input){
-        OptionParser parser = new OptionParser( "yfo" );
-        OptionSet options = parser.parse( "-y", "-f", "-o" );
+    public static void argHandler(String[] input){
+        OptionParser parser = new OptionParser();
+        parser.accepts("y").withRequiredArg();
+        parser.accepts("f").withRequiredArg();
+        parser.accepts("o").withRequiredArg();
+        OptionSet options = parser.parse(input);
 
         String lotto = "C:/greenfox/pkrisz0/week-06/day-03/lotteryfilter/src/main/java/otos.csv";
         String output = "C:/greenfox/pkrisz0/week-06/day-03/lotteryfilter/src/main/java/output.csv";
+
+        String filter = String.valueOf(options.valueOf("y"));
+
+        if (options.has( "y")){
+            openAndWrite(lotto, output, filter);
+        } if (options.has( "f" )) {
+            String from = "C:/greenfox/pkrisz0/week-06/day-03/lotteryfilter/src/main/java/" + String.valueOf(options.valueOf("f"));
+            openAndWrite(from, output, filter);
+        } if (options.has( "o" )){
+            String outputGiven = "C:/greenfox/pkrisz0/week-06/day-03/lotteryfilter/src/main/java/" + String.valueOf(options.valueOf("o"));
+            openAndWrite(lotto, outputGiven, filter);
+        }
+    }
+
+    public static void openAndWrite(String source, String destination, String filter){
+
         List<String[]> lottoContent = new ArrayList<>();
         List<String[]> filteredLotto = new ArrayList<>();
 
-        if (options.has( "y" )){
-            try (CSVReader reader = new CSVReader(new FileReader(lotto))) {
-                lottoContent = reader.readAll();
-            } catch(Exception e){
-                System.out.println("Problem.");
-            }
+        try (CSVReader reader = new CSVReader(new FileReader(source))) {
+            lottoContent = reader.readAll();
+        } catch(Exception e){
+            System.out.println("Problem while reading.");
+        }
 
-            for (int i = 0; i < lottoContent.size(); i++) {
-                if (lottoContent.get(i)[0].startsWith(input[1])){
-                    filteredLotto.add(lottoContent.get(i));
-                }
-            }
-
-            try (CSVWriter writer = new CSVWriter(new FileWriter(output))) {
-                writer.writeAll(filteredLotto);
-            } catch(Exception e){
-                    System.out.println("Problem.");
+        for (int i = 0; i < lottoContent.size(); i++) {
+            if (lottoContent.get(i)[0].startsWith(filter)){
+                filteredLotto.add(lottoContent.get(i));
             }
         }
 
-        options.has( "f" );
-        options.has( "o" );
+        try (CSVWriter writer = new CSVWriter(new FileWriter(destination))) {
+            writer.writeAll(filteredLotto);
+        } catch(Exception e){
+            System.out.println("Problem while writing.");
+        }
     }
 }
+
