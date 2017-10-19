d7b39716a6d64b318f70dffa204eb5ec6621d41e
pkrisz0 <p.krisz89@gmail.com>
2017-10-19 18:49:43 +0200

    missing practice exercises

---
+++ Practice/Test/UniqueCharsTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class UniqueCharsTest {
+
+    ArrayList<String> example;
+
+    @Before
+    public void setUp() throws Exception {
+        example = new ArrayList<>(Arrays.asList("k","m","n","c"));
+    }
+
+    @Test
+    public void uniqueCharacters() throws Exception {
+        assertEquals(example, UniqueChars.uniqueCharacters("kemence"));
+    }
+
+    //static method testnel classon keresztul meghivni a methodot
+}
---
+++ Practice/src/Candy.java
@@ -0,0 +1,6 @@
+public class Candy extends Dessert {
+
+    public Candy(){
+        super(20,10);
+    }
+}
