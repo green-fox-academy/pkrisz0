61a9f4514f1d1c9fb07f6f87a03556636a2ddf8d
pkrisz0 <p.krisz89@gmail.com>
2017-10-20 12:57:28 +0200

    todo filereading and writing methods

---
+++ week-06/day-05/todoapp/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        TodoApp one = new TodoApp();
+        one.add("walk the dog");
+        one.argHandler(args);
+    }
+}
--- week-06/day-05/todoapp/src/main/java/TodoApp.java
+++ week-06/day-05/todoapp/src/main/java/TodoApp.java
@@ -1,39 +1,42 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 public class TodoApp extends ArrayList<Todo> {
     public TodoApp() {
-        welcome();
-    }
-
-    public static void main(String[] args) {
-        TodoApp one = new TodoApp();
-        one.argHandler(args);
     }
 
     public void argHandler(String[] input){
         OptionParser parser = new OptionParser();
-        parser.accepts("l").withRequiredArg();
+        parser.accepts("l");
         parser.accepts("a").withRequiredArg();
         parser.accepts("r").withRequiredArg();
         parser.accepts("c").withRequiredArg();
         OptionSet options = parser.parse(input);
 
         if (options.has("l")) {
-            this.print();
-        } if (options.has("a")){
-            this.add(String.valueOf(options.valueOf("a")));
-        } if (options.has("r")){
-            this.delete((Integer)(options.valueOf("r")));
-        } if (options.has("c")){
-            this.complete((Integer)options.valueOf("c"));
+            this.read();
         }
+        else if (options.has("a")) {
+            this.add(String.valueOf(options.valueOf("a")));
+        }
+        else if (options.has("r")) {
+            this.delete((Integer) (options.valueOf("r")));
+        }
+        else if (options.has("c")) {
+            this.complete((Integer) options.valueOf("c"));
+        } else {
+            welcome();
+        }
+
+        this.write();
     }
 
     public void add(String description){
@@ -48,9 +51,6 @@ public class TodoApp extends ArrayList<Todo> {
         this.get(index - 1).setComplete(true);
     }
 
-    public void print(){
-        System.out.println(this.toString());
-    }
 
     @Override
     public String toString() {
@@ -58,7 +58,7 @@ public class TodoApp extends ArrayList<Todo> {
         for (int i = 0; i < this.size(); i++) {
             sb.append(i + 1);
             sb.append(" - ");
-            sb.append(this.get(i));
+            sb.append(this.get(i).toString());
             sb.append(System.lineSeparator());
         }
         return sb.toString();
@@ -66,13 +66,36 @@ public class TodoApp extends ArrayList<Todo> {
 
     public static void welcome(){
         try {
-            Path filepath = Paths.get("C:/greenfox/pkrisz0/week-06/day - 05/todoapp/src/main/java/usage.txt");
+            Path filepath = Paths.get("C:/greenfox/pkrisz0/week-06/day-05/todoapp/src/main/java/usage.txt");
             List<String> lines = Files.readAllLines(filepath);
             for (int i = 0; i < lines.size(); i++) {
                 System.out.println(lines.get(i));
             }
         } catch (IOException e) {
             System.out.println("Unable to write file: print_usage");
+        }
+    }
+
+    public void write(){
+        ArrayList<String> content = new ArrayList<>();
+        content.add(this.toString());
+        try {
+            Path filepath = Paths.get("C:/greenfox/pkrisz0/week-06/day-05/todoapp/src/main/java/list.txt");
+            Files.write(filepath, content);
+        } catch (IOException e) {
+            System.out.println("Unable to write list file.");
+        }
+    }
+
+    public void read(){
+        try {
+            Path filepath = Paths.get("C:/greenfox/pkrisz0/week-06/day-05/todoapp/src/main/java/list.txt");
+            List<String> list = Files.readAllLines(filepath);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read the list");
         }
     }
 }
