9f99511c947b11e1cc140feee798b5d18727a031
pkrisz0 <p.krisz89@gmail.com>
2017-10-25 11:05:33 +0200

    simbank exercise one complete

--- week-07/day-03/simbank/src/main/java/com/greenfox/pkrisz0/simbank/controller/ControllerExercise1.java
+++ week-07/day-03/simbank/src/main/java/com/greenfox/pkrisz0/simbank/controller/ControllerExercise1.java
@@ -4,13 +4,17 @@ package com.greenfox.pkrisz0.simbank.controller;
 import com.greenfox.pkrisz0.simbank.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class ControllerExercise1 {
 
     @RequestMapping(value="/exercise1")
-    public String exerciseOne(){
-        new BankAccount("Simba", 2000, "lion");
+    public String exerciseOne(Model model){
+        BankAccount one = new BankAccount("Simba", 2000, "lion");
+        model.addAttribute("name", one.getName());
+        model.addAttribute("balance", one.getBalance());
+        model.addAttribute("animalType", one.getAnimalType());
         return "exercise1";
     }
 }
