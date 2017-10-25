7c11d46f83f5c097825682d94085fdcf46ec7c82
pkrisz0 <p.krisz89@gmail.com>
2017-10-25 12:53:53 +0200

    simbank table of accounts complete

---
+++ week-07/day-03/simbank/src/main/java/com/greenfox/pkrisz0/simbank/controller/MultipleAccountController.java
@@ -0,0 +1,22 @@
+package com.greenfox.pkrisz0.simbank.controller;
+
+import com.greenfox.pkrisz0.simbank.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class MultipleAccountController {
+    @RequestMapping(value="/multipleaccounts")
+    public String exerciseOne(Model model){
+        ArrayList<BankAccount> accounts = new ArrayList<>();
+        accounts.add(new BankAccount("Scar", "20000", "lion"));
+        accounts.add(new BankAccount("Zayu", "5000", "bird"));
+        accounts.add(new BankAccount("Rafiki", "10000", "mandrill"));
+        accounts.add(new BankAccount("Gopher", "500", "gopher"));
+        model.addAttribute("accounts", accounts);
+        return "multipleaccounts";
+    }
+}
