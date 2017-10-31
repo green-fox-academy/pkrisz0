6070a119d89c7223fc52819afe357a7f8f2780f1
pkrisz0 <p.krisz89@gmail.com>
2017-10-31 17:26:11 +0100

    todoapp: deletion feature added

--- week-08/day-02/todo/src/main/java/com/greenfox/pkrisz/todo/controller/TodoController.java
+++ week-08/day-02/todo/src/main/java/com/greenfox/pkrisz/todo/controller/TodoController.java
@@ -5,14 +5,11 @@ import com.greenfox.pkrisz.todo.repository.TodoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("todo")
+@RequestMapping("/todo")
 public class TodoController {
 
     @Autowired
@@ -25,14 +22,6 @@ public class TodoController {
     return "todo";
     }
 
-//
-//    @RequestMapping("/done")
-//    public String isDone (Model model, @RequestParam(name = "isActive") boolean done) {
-//        model.addAttribute("todos", todoRepo.findAll());
-//        model.addAttribute("done", done);
-//    return "tododone";
-//    }
-
     @RequestMapping("/add")
     public String add(Model model) {
         model.addAttribute("todo", new Todo());
@@ -42,6 +31,12 @@ public class TodoController {
     @PostMapping("/save")
     public String add(@ModelAttribute Todo todo) {
         todoRepo.save(todo);
+        return "redirect:/todo/list";
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable long id) {
+        todoRepo.delete(id);
         return "redirect:/todo/list";
     }
 }
