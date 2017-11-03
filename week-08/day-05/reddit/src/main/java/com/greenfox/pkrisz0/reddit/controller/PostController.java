2532e8e1a195f0d2942aae0765bc2b23f005f4e7
pkrisz0 <p.krisz89@gmail.com>
2017-11-03 10:22:42 +0100

    reddit adding posts complete

--- week-08/day-05/reddit/src/main/java/com/greenfox/pkrisz0/reddit/controller/PostController.java
+++ week-08/day-05/reddit/src/main/java/com/greenfox/pkrisz0/reddit/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -19,13 +21,19 @@ public class PostController {
 
     @GetMapping({"","/","list"})
     public String list(Model model){
-        model.addAttribute("newpost", new Post());
+        model.addAttribute("posts", postRepo.findAll());
         return "posts";
+    }
+
+    @PostMapping({"/","list"})
+    public String save(@ModelAttribute Post post){
+        postRepo.save(post);
+        return "redirect:/posts";
     }
 
     @GetMapping("/add")
     public String add(Model model){
-        model.addAttribute("posts", postRepo.findAll());
-        return "redirect:/posts";
+        model.addAttribute("post", new Post() );
+        return "add";
     }
 }
